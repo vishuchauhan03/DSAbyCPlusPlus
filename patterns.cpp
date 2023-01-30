0e458372ceaaa2e87fbb5d35585178750e54cefc
vishal chauhan <vishal.eeeeng@gmail.com>
2023-01-31 00:34:25 +0530

    working on patterns

---
+++ patterns.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // Printing Solid Rectangle Of Given Lenght And Breadth
+
+    // int length;
+    // cout<<"Enter Length of Rectangle \n";
+    // cin>>length;
+    // int breadth;
+    // cout<<"Enter Breadth of Rectangle \n";
+    // cin>>breadth;
+    // for(int row=0; row<breadth; row++){
+    //     for (int col = 0; col < length; col++){
+    //         cout<<"* ";   
+    //     }
+    //     cout<<endl;
+    // }
+
+   // Printing Solid Square Of N Side
+
+    // int side;
+    // cout<<"Enter Side of Square \n";
+    // cin>>side;
+    // for(int row=0; row<side; row++){
+    //     for (int col = 0; col < side; col++){
+    //         cout<<"* ";   
+    //     }
+    //     cout<<endl;
+    // }
+
+   // Printing Hollow Rectangle Of Given Length and Breadth
+
+    // int length;
+    // cout<<"Enter Length of Rectangle \n";
+    // cin>>length;
+    // int breadth;
+    // cout<<"Enter Breadth of Rectangle \n";
+    // cin>>breadth;
+    // for(int row=0; row<breadth; row++){
+    //     if(row==0 || row==breadth-1){
+    //         for (int col = 0; col < length; col++){
+    //             cout<<"*";   
+    //         }
+    //     }else{
+    //         cout<<"*";
+    //          for (int col = 0; col < length-2; col++){
+    //             cout<<" ";   
+    //         }
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    // Printing Half Pyramid Of N number Of Rows
+
+    // int row;
+    // cout<<"Enter Number of Rows \n";
+    // cin>>row;
+    // for (int i = 0; i < row; i++)
+    // {
+    //     for (int j = 0; j < i+1; j++)
+    //     {
+    //          cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // Printing Inverted Half Pyramid Of N number Of Rows
+    
+    // int row;
+    // cout<<"Enter Number of Rows \n";
+    // cin>>row;
+    // for (int i = row; i > 0; i--)
+    // {
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //          cout<<"*";
+    //     }
+    //     cout<<endl;    
+    // }
+
+    // Printing Numeric Half Pyramid Of N number Of Rows
+
+    // int row;
+    // cout<<"Enter Number of Rows \n";
+    // cin>>row;
+    // for (int i = 0; i < row; i++)
+    // {
+    //     for (int j = 1; j <= i+1; j++)
+    //     {
+    //          cout<<j;
+    //     }
+    //     cout<<endl;
+    // }
+
+     // Printing Inverted Numeric Half Pyramid Of N number Of Rows
+    
+    // int row;
+    // cout<<"Enter Number of Rows \n";
+    // cin>>row;
+    // for (int i = row; i > 0; i--)
+    // {
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //          cout<<j+1;
+    //     }
+    //     cout<<endl;    
+    // }
+
+}

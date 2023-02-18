aff0b42ee937594682bb58fc7553b9be2099e392
vishal chauhan <vishal.eeeeng@gmail.com>
2023-02-18 14:13:42 +0530

    sdf

---
+++ function.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// fatorial of a number
+long long fact(int n){
+    long long fact=1;
+    for (int i = n; i > 1; i--)
+    {
+        fact*=i;
+    }
+    return fact;
+    
+}
+
+int main(){
+    // int number ;
+    // cout<<"enter the number"<<endl;
+    // cin>>number;
+    // cout<<fact(number);
+    // if(-3){
+    //     cout<<"hello";
+    // }
+    int a=(2,6);
+    cout<<a;
+    return 0;
+}
